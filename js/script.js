04c96257f50d48e838617e04d161af3c7493dfb6
Eng Abdallah Hussein <107323788+A7Joka@users.noreply.github.com>
2025-08-09 12:17:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -375,11 +375,14 @@ if (details['Match-Status'] === 'إنتهت المباراة - ركلات الت
         winner = null;
     }
 
-    let penResult = `${leftTeam['Penalty-Score']} - ${rightTeam['Penalty-Score']}`;
+    let penResult = `${leftTeam['Penalty-Score']}-${rightTeam['Penalty-Score']}`;
   if(winner){
     penInfo = `
-    <div class="info-item w-max">
-        <span class="info-label text-gray-800 dark:text-gray-100">
+    <div class="info-item w-max border border-primary dark:border-primary rounded-2xl p-2 m-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
+            <path id="Combined_Shape" data-name="Combined Shape" d="M3.222,18.778A11,11,0,1,1,18.778,3.222,11,11,0,1,1,3.222,18.778ZM2,11a9,9,0,1,0,9-9A9.01,9.01,0,0,0,2,11Zm8.294,3.707A1,1,0,1,1,11,15,1,1,0,0,1,10.293,14.707ZM10,11V7a1,1,0,1,1,2,0v4a1,1,0,0,1-2,0Z" transform="translate(0 0)" fill="#1eafb2"></path>
+        </svg>
+        <span class="modal-team-name text-gray-800 dark:text-gray-100">
             إنتهت بفوز ${winner} ${penResult} بركلات الترجيح
         </span>
     </div>
@@ -1321,6 +1324,7 @@ export {
   displayStandings,
   showNewsArticle,
 };
+
 
 
 
